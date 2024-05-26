71076a3a0ffcb9f68fc6b95c34e37558d92df277
shun.sugimoto <shun.sugimoto@e-seikatsu.co.jp>
2024-05-26 17:49:16 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
+
+
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -17,13 +19,17 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
-  return (
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
+
+    return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
         ))}
       </ul>
       <div>
